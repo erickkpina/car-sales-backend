68b4ac8d4618d8f69dffd6b3f8fc70dd9cae59a8
erick <a22006182@alunos.ulht.pt>
2023-10-18 14:57:54 +0100

    Adding elements to Car model

--- dealership/models.py
+++ dealership/models.py
@@ -4,9 +4,20 @@ from django.db import models
 # Create your models here.
 
 class Car(models.Model):
-    name = models.CharField(max_length=120)
-    brand = models.CharField(max_length=120)
-    price = models.IntegerField()
+    name = models.CharField(max_length=120, default='')
+    brand = models.CharField(max_length=120, default='')
+    category = models.CharField(max_length=120, default='')
+    gearbox = models.CharField(max_length=120, default='')
+    fuel = models.CharField(max_length=120, default='')
+    guarantee = models.CharField(max_length=120, default='')
+    state_of_use = models.CharField(max_length=120, default='')
+    year = models.IntegerField(default=0)
+    mileage = models.IntegerField(default=0)
+    cylinder_capacity = models.IntegerField(default=0)
+    power = models.IntegerField(default=0)
+    maximum_capacity = models.IntegerField(default=0)
+    doors = models.IntegerField(default=0)
+    price = models.IntegerField(default=0)
 
-    def _str_(self):
+    def __str__(self):
         return self.name

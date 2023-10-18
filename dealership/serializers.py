0828cb8f5768d402dd388aace1838009bae64449
erick <a22006182@alunos.ulht.pt>
2023-10-18 14:55:20 +0100

    rebasing

--- dealership/serializers.py
+++ dealership/serializers.py
@@ -5,4 +5,4 @@ from .models import Car
 class CarSerializer(serializers.ModelSerializer):
     class Meta:
         model = Car
-        fields = ('id', 'name', 'brand', 'price')
+        fields = '__all__'
